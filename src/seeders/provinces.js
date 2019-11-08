20630c39a84b58598cb759946a4c23987c42acb5
Karl MUSINGO <karlemusingo@gmail.com>
2019-11-09 00:07:12 +0200

    adding provinces their ancient provinces to the provinces seeder

--- src/seeders/provinces.js
+++ src/seeders/provinces.js
@@ -1,4 +1,4 @@
-//superficie en (km²) et	Population en 2015
+// //superficie en (km²) et	Population en 2015
 // source: https://fr.wikipedia.org/wiki/Provinces_de_la_r%C3%A9publique_d%C3%A9mocratique_du_Congo
 // crapping script 
 // document.querySelectorAll('tr').forEach((value, index)=>{
@@ -38,3 +38,36 @@ export default [
   {nom: "Tshopo", chefLieu: "Kisangani", superficie: 199567, population: 2352000},
   {nom: "Tshuapa", chefLieu: "Boende", superficie: 132957, population: 1600000},
 ];
+
+// les nouvelles provinces avec leurs provinces de provenance 
+export const provincesWithAncien = [
+  {nom: "Equateur (anc. Equateur)", chefLieu : "Mbandaka", langue  : "Lingala", superficie : 103902},
+  {nom: 'Sud-Ubangui (anc. Equateur)', chefLieu : 'Gemena', langue  : 'Lingala', superficie : 51648,},
+  {nom: 'Nord-Ubangui (anc. Equateur)', chefLieu : 'Gbadolite', langue  : 'Lingala', superficie : 56644},
+  {nom: 'Mongala (anc. Equateur)', chefLieu : 'Lisala', langue  : 'Lingala', superficie : 58141},
+  {nom: 'Bas-Uele (anc.Province Orientale)', chefLieu : 'Buta', langue  : 'Lingala', superficie :148331},
+  {nom: 'Haut-Uele (anc.Province Orientale)', chefLieu : 'Isiro', langue  : 'Lingala', superficie : 98683},
+  {nom: 'Ituri (anc.Province Orientale)', chefLieu : 'Bunia', langue  : 'Swahili', superficie : 65658},
+  {nom: 'Kongo Central (anc.Bas-Congo)', chefLieu : 'Matadi', langue  : 'Kikongo', superficie : 53920},
+  {nom: 'Kinshasa', chefLieu : 'Kinshasa', langue  : 'Lingala', superficie : 9965},
+  {nom: 'Mai-Ndombe(anc.Bandundu)', chefLieu : 'Inongo', langue: 'Lingala', superficie : 127465,},
+  {nom: 'Kwilu (anc.Bandundu)', chefLieu : 'Bandundu', langue: 'Kikongo', superficie : 78219,},
+  {nom: 'Kwango (anc.Bandundu)', chefLieu : 'Kenge', langue: 'Kikongo', superficie : 89974,},
+  {nom: 'Tshuapa (anc.Equateur)', chefLieu : 'Boende', langue: 'Lingala', superficie : 132957,},
+  {nom: 'Tshopo (anc.Province Orientale)', chefLieu : 'Kisangani', langue: 'Swahili', superficie : 199567,},
+  {nom: 'Sankuru (anc.Kasaï Oriental)', chefLieu : 'Lusambo', langue: 'Lotetela', superficie : 104331,},
+  {nom: 'Kasaï (anc.Kasaï Occidental)', chefLieu : 'Tshikapa', langue: 'Tshiluba', superficie : 95631,},
+  {nom: 'Kasaï Oriental(anc.Kasaï Oriental)', chefLieu : 'Mbuji-Mayi', langue: 'Tshiluba', superficie : 9481,},
+  {nom: 'Kasaï Central (anc.Kasaï Occidental)', chefLieu : 'Kananga', langue: 'Tshiluba', superficie : 60958,},
+  {nom: 'Lomani (anc.Kasaï Oriental)', chefLieu : 'Kabinda', langue: 'Tshiluba', superficie : 56426,},
+  {nom: 'Nord Kivu', chefLieu : 'Goma', langue: 'Swahili', superficie : 59483,},
+  {nom: 'Sud Kivu', chefLieu : 'Bukavu', langue: 'Swahili', superficie : 65070,},
+  {nom: 'Maniema', chefLieu : 'Kindu', langue: 'Swahili', superficie : 132520,},
+  {nom: 'Tanganyika(anc.Katanga)', chefLieu : 'Kalemi', langue: 'Swahili', superficie : 134940,},
+  {nom: 'Haut-Katanga(anc.Katanga)', chefLieu : 'Lubumbashi', langue: 'Swahili', superficie : 132425,},
+  {nom: 'Lualaba (anc.Katanga)', chefLieu : 'Kolwezi', langue: 'Swahili', superficie : 121308,},
+  {nom: 'Haut-Lomani(anc.Katanga)', chefLieu : 'Kamina', langue: 'Swahili', superficie : 108204}
+];
+
+
+
